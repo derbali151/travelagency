bae4aa1845f6429d99a8b79f7518f4add45da637
Abdelkader <hassen.derbali999@gmail.com>
2019-11-11 20:37:39 +0100

    jpa vaidation response entity

---
+++ src/main/java/com/ditraacademy/travelagency/utility/ErrorResponseModel.java
@@ -0,0 +1,12 @@
+package com.ditraacademy.travelagency.utility;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+@Getter
+@Setter
+@AllArgsConstructor
+public class ErrorResponseModel {
+
+    private String message;
+}
